ecb1e49f3d9120598ee04ec70ed41b92b2952c08
Juan José Denis Corrales <jjdenis@gmail.com>
2015-03-15 21:05:12 +0100

    Show data in response

--- run-demo.py
+++ run-demo.py
@@ -14,30 +14,44 @@ static_folder = os.path.join(PWD, "static")
 app = Eve(static_folder=static_folder)
 
 
+import logging
+# log = logging.getLogger('werkzeug')
+
 # These funcitions show the full request and response.
+
+# logging.basicConfig(level=logging.WARNING)
+
 
 @app.before_request
 def before():
-    print
+    print('')
     print '  > %s %s' % (request.method, request.url)
     for k, v in request.headers:
-        print wrap_header(k, v)
-    print
+        print wrap_header(k, v, '>')
+
+    print(request.headers['content-type'])
+    print('')
 
 
 @app.after_request
 def after(response):
     print '  < %s' % response.status
     for k, v in response.headers:
-        print wrap_header(k, v)
+        print wrap_header(k, v, '<')
+
+    if response.mimetype == 'application/json':
+        print response.data
+
     return response
 
 
-def wrap_header(header, value):
-    interm = '  > %s: %s' % (header, value)
-    indent = len(header) + 6
-    wrapped = textwrap.fill(interm, 60, subsequent_indent=' ' * indent)
-    return wrapped
+def wrap_header(header, value, sign):
+    interm = '%s: %s' % (header, value)
+    indent = len(header) + 2
+    wrapped = textwrap.wrap(interm, 60, subsequent_indent=' ' * indent)
+    spaced = ['  {} {}'.format(sign, l) for l in wrapped]
+
+    return '\n'.join(spaced)
 
 
 def main():
