75b543685afc7fc0b2262021ef9d6d79749f22e5
Juan José Denis Corrales <jjdenis@gmail.com>
2015-04-15 19:58:51 +0200

    Separate curl-like logging

--- run-demo.py
+++ run-demo.py
@@ -2,11 +2,7 @@
 from eve import Eve
 import os
 
-from flask import request
-
-import textwrap
-import json
-import pprint
+import curl
 
 # set folder for static data
 PWD = os.environ.get("PWD")
@@ -16,64 +12,8 @@ app = Eve(static_folder=static_folder)
 
 LINE_LENGTH = 80
 
-# import logging
-# log = logging.getLogger('werkzeug')
-# logging.basicConfig(level=logging.WARNING)
-
-# These functions show the full request and response.
-
-
-@app.before_request
-def before():
-    print('\n\n')
-    print '< %s %s' % (request.method, request.url)
-    for k, v in request.headers:
-        print wrap_header(k, v, '<')
-
-    # print(request.headers['content-type'])
-    # print('')
-
-    if request.mimetype == 'application/json':
-        print wrap_json(request.data, '<')
-
-    print('')
-
-
-@app.after_request
-def after(response):
-    print '> %s' % response.status
-    for k, v in response.headers:
-        print wrap_header(k, v, '>')
-
-    if response.mimetype == 'application/json':
-        print wrap_json(response.data, '>')
-
-    return response
-
-
-def wrap_json(json_string, sign):
-    pprinted = pprint.pformat(
-        json.loads(json_string), width=LINE_LENGTH).split('\n')
-    spaced = ['{} {}'.format(sign, l) for l in pprinted]
-    finished = []
-    for line in spaced:
-        indent = line.find(':') + 4
-        line_wrapped = textwrap.fill(
-            line, LINE_LENGTH, subsequent_indent=' ' * indent)
-        finished.append(line_wrapped)
-        # spaced = ['{} {}'.format(sign, l) for l in line_wrapped]
-
-    return '\n'.join(finished)
-
-
-def wrap_header(header, value, sign):
-    interm = '%s: %s' % (header, value)
-    indent = len(header) + 2
-    wrapped = textwrap.wrap(
-        interm, LINE_LENGTH, subsequent_indent=' ' * indent)
-    spaced = ['{} {}'.format(sign, l) for l in wrapped]
-
-    return '\n'.join(spaced)
+# This adds curl-like logging
+curl.add_curl(app)
 
 
 def main():
